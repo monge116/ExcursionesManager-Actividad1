766ddaa53a160c564cb381d13b0cb6912bd38b6d
rodrigo <rodrigorodriguezmonge@gmail.com>
2024-10-25 14:21:57 +0200

    Excursion y ExcursionDao

--- src/main/java/excursiones/modelo/dao/ExcursionDao.java
+++ src/main/java/excursiones/modelo/dao/ExcursionDao.java
@@ -1,5 +1,16 @@
 package excursiones.modelo.dao;
 
-public class ExcursionDao {
-	Excursion
+import java.util.List;
+
+import excursiones.modelo.entities.Excursion;
+
+public interface ExcursionDao {
+	Excursion findById(int id);
+	List<Excursion> findAll();
+	int insertOne(Excursion excursion);
+	int updateOne(Excursion excursion);
+	List<Excursion> findByCreados(String estado);
+	List<Excursion> findByCancelados(String estado);
+	List<Excursion> findByTerminado(String estado);
+	List<Excursion> findByDestados(String destacado);
 }
--- src/main/java/excursiones/modelo/entities/Excursion.java
+++ src/main/java/excursiones/modelo/entities/Excursion.java
@@ -2,6 +2,7 @@ package excursiones.modelo.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -42,4 +43,155 @@ public class Excursion implements Serializable{
 	@JoinColumn(name="id_familia")
 	private Familia familia;
 	*/
+	
+	
+	public Excursion() {
+		super();
+	}
+
+	public Excursion(long idExcursion, String descripcion, String origen, String destino, Date fechaExcursion,
+			int duracion, String estado, String destacado, int aforoMaximo, int precioUnitario, String imagen,
+			Date fechaAlta) {
+		super();
+		this.idExcursion = idExcursion;
+		this.descripcion = descripcion;
+		this.origen = origen;
+		this.destino = destino;
+		this.fechaExcursion = fechaExcursion;
+		this.duracion = duracion;
+		this.estado = estado;
+		this.destacado = destacado;
+		this.aforoMaximo = aforoMaximo;
+		this.precioUnitario = precioUnitario;
+		this.imagen = imagen;
+		this.fechaAlta = fechaAlta;
+	}
+
+	public long getIdExcursion() {
+		return idExcursion;
+	}
+
+	public void setIdExcursion(long idExcursion) {
+		this.idExcursion = idExcursion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public Date getFechaExcursion() {
+		return fechaExcursion;
+	}
+
+	public void setFechaExcursion(Date fechaExcursion) {
+		this.fechaExcursion = fechaExcursion;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getDestacado() {
+		return destacado;
+	}
+
+	public void setDestacado(String destacado) {
+		this.destacado = destacado;
+	}
+
+	public int getAforoMaximo() {
+		return aforoMaximo;
+	}
+
+	public void setAforoMaximo(int aforoMaximo) {
+		this.aforoMaximo = aforoMaximo;
+	}
+
+	public int getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(int precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idExcursion);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Excursion))
+			return false;
+		Excursion other = (Excursion) obj;
+		return idExcursion == other.idExcursion;
+	}
+
+	@Override
+	public String toString() {
+		return "Excursion [idExcursion=" + idExcursion + ", descripcion=" + descripcion + ", origen=" + origen
+				+ ", destino=" + destino + ", fechaExcursion=" + fechaExcursion + ", duracion=" + duracion + ", estado="
+				+ estado + ", destacado=" + destacado + ", aforoMaximo=" + aforoMaximo + ", precioUnitario="
+				+ precioUnitario + ", imagen=" + imagen + ", fechaAlta=" + fechaAlta + "]";
+	}
+	
+	
+	
+	
+	
 }
